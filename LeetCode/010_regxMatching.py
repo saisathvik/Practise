0c780dfb5ad8057d6d99cf5fcb51d036370042aa
saisathvik <saisathvik.y@gmail.com>
2018-08-23 20:24:23 +0530

    Update 010_regxMatching.py

--- LeetCode/010_regxMatching.py
+++ LeetCode/010_regxMatching.py
@@ -9,13 +9,17 @@ class Solution:
 
         def dp(i,j):
             if (i,j) not in breaks:
+                # if both points dont reach end in the same check, it is False
                 if j == len(p):
                     ans = (i == len(s))
                 else:
+                    #f_m is the bool of the first character match
                     f_m = (i < len(s)) and p[j] in {s[i],"."}
+                    #if the next char is *, there are zero reps or more. One of these branches has to match (for True)
                     if j + 1 < len(p) and p[j + 1] == "*":
                         ans = dp(i, j+2) or (f_m and dp(i+1, j))
                     else:
+                    # if the next char is not *, move both points by one place
                         ans = f_m and dp(i+1, j+1)
 
                 breaks[i, j] = ans
