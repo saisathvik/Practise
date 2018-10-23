5fdcfc997ca0680bdf73bbab477c634e417af76c
saisathvik <saisathvik.y@gmail.com>
2018-10-23 17:51:22 +0530

    Delete InvalidWeatherException.java

--- traffic2/src/main/java/in/geektrust/traffic/common/InvalidWeatherException.java
+++
@@ -1,8 +0,0 @@
-package in.geektrust.traffic.common;
-
-public class InvalidWeatherException extends Exception {
-
-    public InvalidWeatherException(String message){
-        super(message);
-    }
-}
